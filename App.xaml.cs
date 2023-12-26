f73a3f20a83ab2d2ad80930b63d21d2a479f8aec
serg-225 <Usanov.serg@gmail.com>
2023-12-26 15:05:41 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Capital
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
