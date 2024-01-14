cbd5cae1c6dad4928156516af76c1875365baf12
ivkt <ivkt@yandex.ru>
2024-01-14 13:12:33 +0300

    Data_Depo

--- Entity/Data.cs
+++ Entity/Data.cs
@@ -1,4 +1,5 @@
-﻿using Capital.Enams;
+﻿using Capital.Enums;
+using Capital.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,124 +13,111 @@ namespace Capital.Entity
         public Data(decimal depoStart, StrategyType strategyType)
         {
             StrategyType = strategyType;
-
             Depo = depoStart;
         }
 
-        #region Propertirs =================================
-
+        #region Properties ======================================
         public StrategyType StrategyType { get; set; }
-
-
         public decimal Depo
         {
             get => _depo;
-
             set
             {
                 _depo = value;
                 ResultDepo = value;
             }
+
         }
         decimal _depo;
-
         /// <summary>
         /// Результат эквити (депо)
         /// </summary>
         public decimal ResultDepo
         {
             get => _resultDepo;
-
             set
             {
                 _resultDepo = value;
-
-                Profit = ResultDepo - Depo;
-
-                PercentProfit = Profit * 100 / Depo;
-
-                LisrEquity.Add(ResultDepo);
-
-                CalcDrawDown();
+                Profit = ResultDepo - Depo; // расчет Профита
+                PercentProfit = Profit * 100 / Depo;// расчет профита в прцентах
+                ListEquity.Add(ResultDepo);// добавляем в Лист каждый раз раз новое значение при новой сделки
+                CalcDrawDown();// запускаем вычисление макс просадки
             }
+
         }
         decimal _resultDepo;
 
         public decimal Profit { get; set; }
 
-        /// <summary>
-        /// Относительный профит в процентах
+        ///<summary>
+        ///Относительный профит в процентах
         /// </summary>
+
         public decimal PercentProfit { get; set; }
-
-        /// <summary>
-        /// Максимальная абсолбтная просадка в деньгах
+        ///<summary>
+        ///Максимальная абсолютная просадка в деньгах
         /// </summary>
-        public decimal MaxDrawDoun
+        public decimal MaxDrawDown
         {
-            get => _maxDrawDoun;
-
+            get => _maxDrawDown;
             set
             {
-                _maxDrawDoun = value;
-
-                CalcPercentDown();
+                _maxDrawDown = value;
+                CalcPercentDrawDown();// вызываем расчет относительной просадки, когда будем обновлять максимальную абсолютную просадку
             }
         }
-        decimal _maxDrawDoun;
+        decimal _maxDrawDown;
 
-        /// <summary>
-        /// Максимальная относительная просадка в процентах
+        ///<summary>
+        ///Максимальная относительная просадка в процентах
         /// </summary>
+
         public decimal PercentDrawDown { get; set; }
-
         #endregion
 
-        #region Fields =========================================
+        #region ================== Fields ==============
+        // список, фиксирующий точки(значения), в том числе и  _мах и _мин, (хранит значения действующего депо для каждой сделки)
+        List<decimal> ListEquity = new List<decimal>();
 
-        List<decimal> LisrEquity = new List<decimal>();
-
-        private decimal _max = 0;
-
-        private decimal _min = 0;
-
+        decimal _min = 0;
+        decimal _max = 0;
 
         #endregion
-
-        #region Methods ==========================================
-
+        #region ================== Methods =============
         public List<decimal> GetListEquity()
         {
-            return LisrEquity;
+            return ListEquity;
         }
 
-        private void CalcDrawDown()
+        private void CalcDrawDown()//метод для вычисления просадки
         {
             if (_max < ResultDepo)
             {
                 _max = ResultDepo;
                 _min = ResultDepo;
             }
-
             if (_min > ResultDepo)
             {
                 _min = ResultDepo;
 
-                if(MaxDrawDoun < _max - _min)
+                if (MaxDrawDown < _max - _min)
                 {
-                    MaxDrawDoun = _max - _min;
+                    MaxDrawDown = _max - _min;
                 }
             }
         }
-
-        private void CalcPercentDown()
+        private void CalcPercentDrawDown() //расчет относительной просадки
         {
-            if (ResultDepo == 0) ResultDepo = 1;  //Исключил 0 в ResultDepo
-            decimal percent = MaxDrawDoun * 100 / ResultDepo;
-
+            decimal percent = MaxDrawDown * 100 / ResultDepo;// расчет процента просадки
             if (percent > PercentDrawDown) PercentDrawDown = Math.Round(percent, 2);
+
         }
+
 
         #endregion
     }
+
+
+
+
 }
