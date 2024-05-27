d530ae7618692ba00f4aae0e48a4f3c377fb8820
Анатолий <aem3d@mail.ru>
2024-05-27 23:26:41 +0300

    02

--- obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -149,7 +149,7 @@ namespace Capital {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Capital;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Capital;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
