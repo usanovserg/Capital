0517caed4881dfbf715a1c6cd981ad6b044cb1d9
Evgeniy_Toptalov <toptalov92@mail.ru>
2024-05-30 19:43:50 +0300

    DZ8

--- obj/Debug/net6.0-windows/Capital.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/Capital.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Capital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d386ebf6c75ace821eb2781024cc55d80b6f08cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea0d878b328299708800f1f7e52231eda398c2d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Capital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Capital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
