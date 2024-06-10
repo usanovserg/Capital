4740927252e1ebe68fd44b9c76f9e35fe42d0390
Marat198537 <amg4500@gmail.com>
2024-06-10 14:49:16 +0300

    Домашнее щадание к уроку 1-9

--- obj/Debug/net6.0-windows/Capital.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/Capital.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Capital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d386ebf6c75ace821eb2781024cc55d80b6f08cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8d9993f5802b166f761eba64650e62478d240fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Capital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Capital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
