be2614d298ec8930aff2af7493d6ba85590ac167
alvlts <alvlts@mail.ru>
2025-12-13 10:43:18 +0300

    Добавил класс Data и Enum

---
+++ Entity/Data.cs
@@ -0,0 +1,75 @@
+﻿using Capital.Enams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capital.Entity
+{
+    public class Data
+    {
+        public Data(decimal depoStart, StrategyType strategyType)
+        {
+            StrategyType = strategyType;
+
+            Depo = depoStart;
+
+        }
+
+        #region Properties #############################################
+
+        public StrategyType StrategyType { get; set; }
+
+        public decimal Depo 
+        {
+            get => _depo;
+
+            set
+            {
+                _depo = value;
+                 ResultDepo = value;
+            }
+        }
+        decimal _depo;
+
+
+        public decimal ResultDepo 
+        {
+            get => _resultDepo ;
+
+            set
+            {
+                _resultDepo = value;
+            }
+        }
+        decimal _resultDepo;
+
+
+        public decimal Profit { get; set; }
+
+        /// <summary>
+        /// Относительный профит в процентах
+        /// </summary>
+        public decimal PercentProfit { get; set; }
+
+
+        
+
+        public decimal MyProperty
+        {
+            get => _maxDrawDown; 
+
+            set
+            {
+                _maxDrawDown = value;
+            }
+        }
+        private decimal _maxDrawDown;
+
+                      
+        #endregion
+
+
+    }
+}
