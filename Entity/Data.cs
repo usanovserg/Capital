665590bde20bde0597a0777e49d0f10e29bbe324
AndreiReprintsev <Randrew8313@gmail.com>
2025-12-15 22:26:39 +0300

    Урок 1.8

---
+++ Entity/Data.cs
@@ -0,0 +1,97 @@
+﻿using Capital.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capital.Entity
+{
+    public class Data
+    {
+        public Data(decimal depoStart, StrategyType strategyType) 
+        {
+            StrategyType = strategyType;
+            Depo = depoStart;
+        }
+
+        #region Properties =============================================
+
+        public StrategyType StrategyType { get; set; }
+
+        public decimal Depo 
+        {
+            get
+            {
+                return _depo;            
+            }
+
+            set 
+            {
+                _depo = value;
+                ResultDepo = value;
+            }
+        
+        }
+        decimal _depo;
+
+        public decimal ResultDepo
+        {
+            get
+            {
+                return _resultDepo;
+            }
+
+            set
+            {
+                _resultDepo = value;
+            }
+
+        }
+        decimal _resultDepo;
+
+        public decimal Profit { get; set; }
+
+        /// <summary>
+        /// Относительный профит в процентах
+        /// </summary>
+        public decimal PercentProfit { get; set; }
+
+        /// <summary>
+        /// Максимальная абсолютная просадка в деньгах
+        /// </summary>
+        public decimal MaxDrawDown 
+        {
+            get
+            {
+                return _maxDrawDown;
+            }
+
+            set
+            {
+                _maxDrawDown = value;
+            }
+        }
+        decimal _maxDrawDown;
+
+        /// <summary>
+        /// Максимальная относительная просадка в процентах
+        /// </summary>
+        public decimal PercentDrawDown { get; set; }
+
+
+
+
+        #endregion =====================================================
+
+        #region Fields =================================================
+
+        #endregion =====================================================
+
+        #region Methods ================================================
+
+        #endregion =====================================================
+
+    }
+}
