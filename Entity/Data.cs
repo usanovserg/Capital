ac9b093e43607de1962321b823292a4f9bb36f0f
Глеб Ачуров <achurovgg@mail.ru>
2025-12-23 19:30:59 +0300

    /

---
+++ Entity/Data.cs
@@ -0,0 +1,86 @@
+﻿using Capital.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capital.Entity
+{
+    public class Data
+    {
+
+        public Data(decimal depoStart ,StrategyType strategyType) 
+        {
+            StrategyType = strategyType;
+
+            Depo=depoStart;
+        }
+        #region  Properties =====================================
+
+        public StrategyType StrategyType {  get; set; }
+
+        public decimal Depo
+        {
+            get => _depo;
+
+            set
+            {
+                _depo = value;
+                ResultDepo = value;
+            }
+        }
+        decimal _depo;
+
+        public decimal ResultDepo
+        {
+            get => _resultDepo;
+
+            set
+            {
+                _resultDepo = value;
+            }
+        }
+        decimal _resultDepo;
+
+        public decimal Profit { get; set; }
+        /// <summary>
+        /// Относительный профит в процентах
+        /// </summary>
+        public decimal PercentProfit { get; set; }
+        /// <summary>
+        /// max абсолютная просадка в деньгах
+        /// </summary>
+        public decimal MaxDrawDown
+        {
+            get => _maxDrawDown;
+
+            set
+            {
+                _maxDrawDown = value;
+            }
+        }
+        decimal _maxDrawDown;
+
+        /// <summary>
+        /// max относительная просадка
+        /// </summary>
+        public decimal PercentDrawDown { get; set; }
+        
+
+        #endregion
+
+        #region  Fields =====================================
+
+        #endregion
+
+
+
+        #region  Methods =====================================
+
+        #endregion
+
+
+
+    }
+}
