09e715d973319871567430d46193289ff8d35346
dkrasilnikov <krsdm@ya.ru>
2025-12-28 22:24:00 +0300

    расчет просадки

--- Entity/StrategyData.cs
+++ Entity/StrategyData.cs
@@ -4,23 +4,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Navigation;
 
 namespace Capital.Entity
 {
     internal class StrategyData
     {
-        #region Properties ===================================
-        public StrategyType StrategyType { get; set; }
-        public decimal StartDepo { get; set; }
-        public decimal ResultDepo { get; set; }
-        public decimal Profit { get => ResultDepo - StartDepo; }
-        public decimal PersentProfit { get => Profit * 100 / StartDepo; }
-        public decimal MaxDrawdown { get; set; }
-        public decimal PercentMaxDrawdown { get; set; }
+        #region Fields =======================================
+        private decimal _resultDepo;
+        private List<decimal> _equity = new List<decimal>();
+        private decimal _min = 0;
+        private decimal _max = 0;
         #endregion
 
 
-        #region Fields =======================================
+        #region Properties ===================================
+        public StrategyType StrategyType { get; set; }
+        public decimal StartDepo { get; set; }
+        public decimal ResultDepo
+        { 
+            get { return _resultDepo; }
+            set 
+            { 
+                _resultDepo = value;
+                _equity.Add(value);
+                Profit = value - StartDepo; 
+                PersentProfit = Math.Round(Profit * 100 / StartDepo, 2);
+                CalculateDrawdown();
+                CalculatePercentDrawdown();
+            } 
+        }
+        public decimal Profit { get; set; }
+        public decimal PersentProfit { get; set; }
+        public decimal MaxDrawdown { get; set; }
+        public decimal PercentMaxDrawdown { get; set; }
         #endregion
 
 
@@ -33,6 +50,27 @@ namespace Capital.Entity
 
 
         #region Methods =====================================
+        private void CalculateDrawdown()
+        {
+            if (_max < ResultDepo)
+            {
+                _max = ResultDepo;
+                _min = ResultDepo;
+            }
+            if (_min > ResultDepo)
+            {
+                _min = ResultDepo;
+                if (MaxDrawdown < _max - _min)
+                    MaxDrawdown = _max - _min;
+            }
+        }
+
+        private void CalculatePercentDrawdown()
+        {
+            var percent = MaxDrawdown * 100 / ResultDepo;
+            if (percent > PercentMaxDrawdown)
+                PercentMaxDrawdown = Math.Round(percent, 2);
+        }
         #endregion
     }
 }
